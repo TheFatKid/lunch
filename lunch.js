3e842e3b216274c73f279c0d8045de718ce692b8
TheFatKid <tobymadsen@gmail.com>
2021-01-04 20:55:35 +0100

    Update lunch.js

--- lunch.js
+++ lunch.js
@@ -31,6 +31,32 @@
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
   document.getElementById("currentTime").innerHTML = time;
- 
+
+  function dayOfTheWeek() {
+    switch (new Date().getDay()) {
+      case 0:
+        currentDay = "Today is Sunday, and today you eat at home!";
+        break;
+      case 1:
+        currentDay = "Today is Monday and the menu is: ";
+        break;
+      case 2:
+        currentDay = "Today is Tuesday and the menu is: Coq au vin with mashed potatoes.";
+        break;
+      case 3:
+        currentDay = "Today it is Wednesday and the menu is: Thai beef salad with glass noodle salad.";
+        break;
+      case 4:
+        currentDay = "Today it is Thursday and the menu is: Burning love with mash and bacon.";
+        break;
+      case 5:
+        currentDay = "Today it is Friday and the menu is: Mexican day with spinach tortillas and whatever else belongs to.";
+        break;
+      case 6:
+        currentDay = "Saturday, and today you eat at home!";
+      }
+    }
+      document.getElementById("Day").innerHTML = currentDay;
+
 }, 1000);
 })();
